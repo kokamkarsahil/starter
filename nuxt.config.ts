56cb357f8be1b89231c4ad5e2c68da1922cc8164
Sahil <sahil@kokamkar.com>
2023-05-08 16:34:33 +0000

    Reproducible Step
    Signed-off-by: Sahil <sahil@kokamkar.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  extends: '@nuxt-themes/elements',
   modules: ['@nuxt/content'],
   content: {
     documentDriven: true
